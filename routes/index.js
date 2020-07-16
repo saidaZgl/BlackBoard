3095bbcd5ec31c1f64c1e139576d75adb5bbfd2d
Saida Zghal <zghal.saida@orange.fr>
2020-07-16 14:47:29 +0200

    first commit

---
+++ routes/index.js
@@ -0,0 +1,60 @@
+var express = require('express');
+var router = express.Router();
+
+var articleModel = require('../models/articles')
+var orderModel = require('../models/orders')
+
+/* GET home page. */
+router.get('/', function(req, res, next) {
+  res.render('index');
+});
+
+/* GET tasks page. */
+router.get('/tasks-page', function(req, res, next) {
+  res.render('tasks');
+});
+
+/* GET Messages page. */
+router.get('/messages-page', function(req, res, next) {
+  res.render('messages');
+});
+
+/* GET Users page. */
+router.get('/users-page', function(req, res, next) {
+  res.render('users');
+});
+
+/* GET Catalog page. */
+router.get('/catalog-page', async function(req, res, next) {
+
+  var articles = await articleModel.find();
+
+  res.render('catalog', {articles});
+});
+
+/* GET Orders-list page. */
+router.get('/orders-list-page', async function(req, res, next) {
+
+  var orders = await orderModel.find();
+  
+  res.render('orders-list', {orders});
+});
+
+/* GET Order detail page. */
+router.get('/order-page', async function(req, res, next) {
+
+  var order = await orderModel.findById(req.query.id)
+                              .populate('articles')
+                              .exec()
+
+  res.render('order', {order});
+});
+
+/* GET chart page. */
+router.get('/charts', function(req, res, next) {
+  res.render('charts');
+});
+
+
+
+module.exports = router;
